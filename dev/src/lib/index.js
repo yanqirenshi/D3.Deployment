6f2b53a41328c75ab8a9c6e8e434e19c1d4c0740
yanqirenshi <yanqirenshi@gmail.com>
2021-11-06 10:28:59 +0900

    var 0.2.0

--- dev/src/lib/index.js
+++ dev/src/lib/index.js
@@ -1,5 +1,8 @@
 import D3Deployment, {Camera} from './components/D3Deployment.js';
 
-export { Camera };
+import Rectum from './js/Rectum.js';
+
+// export { Camera };
+export { Rectum };
 
 export default D3Deployment;
